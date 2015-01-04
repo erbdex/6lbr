c319c89104de7360a9d5817165f8c58386f6928e
root <erbdex@gmail.com>
2015-01-05 02:34:55 +0530

    bug fix

--- doc/install_6lbr.sh
+++ doc/install_6lbr.sh
@@ -1,7 +1,6 @@
 sudo su -
 cd
 apt-get install -y terminator htop git libncurses5-dev bridge-utils 
-git clone http://github.com/erbdex/6lbr.git
 cd 6lbr
 git checkout 1410_working
 git pull origin 1410_working
@@ -12,6 +11,6 @@ make tools
 make plugins
 make install
 
-cp ../../docs/6lbr.conf /etc/6lbr/
+cp ../../doc/6lbr.conf /etc/6lbr/
 
 
